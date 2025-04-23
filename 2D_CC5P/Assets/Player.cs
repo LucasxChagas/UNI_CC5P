38d69e9139c02c0226f9c0c4787f663868303e1d
Lucas X. C. <contatolchagas@gmail.com>
2025-04-23 20:07:12 -0300

    Aula #3 - Animator States

--- 2D_CC5P/Assets/Player.cs
+++ 2D_CC5P/Assets/Player.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private Vector2 movementInputs;
     [SerializeField] private float movementSpeed;
-    [SerializeField] SpriteRenderer enemy;
+    [SerializeField] private Animator anim;
     private Rigidbody2D rb;
+
+    private bool IsWalking;
 
     private void Awake()
     {
@@ -21,6 +24,8 @@ public class Player : MonoBehaviour
     void Update()
     {
         GetInputs();
+        UpdateAnimator();
+        UpdateOrientation();
     }
 
     private void FixedUpdate()
@@ -36,59 +41,28 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        // Movimento por Translate
-        //transform.Translate(new Vector3(movementInputs.x, movementInputs.y, 0).normalized *
-        //    Time.deltaTime * movementSpeed);
-
-        // Movimento por Velocity (rigidBody)
         rb.linearVelocity = movementInputs * movementSpeed;
-
-        //rb.AddForce(movementInputs * movementSpeed, ForceMode2D.Force);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void UpdateAnimator()
     {
-        if(collision.gameObject.CompareTag("Switch"))
+        if(rb.linearVelocity.magnitude > 0)
+            IsWalking = true;
+        else
+            IsWalking = false;
+
+        anim.SetBool("IsWalking", IsWalking);
+    }
+
+    private void UpdateOrientation()
+    {
+        if(movementInputs.x > 0) // EstÃ¡ indo para direita...
         {
-            Debug.Log("Começou a Colisão");
-            enemy.color = Color.black;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(movementInputs.x < 0) // EstÃ¡ indo para esquerda...
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
         }
     }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Switch"))
-        {
-            Debug.Log("Permanece na Colisão");
-
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Switch"))
-        {
-            Debug.Log("Saiu da Colisão");
-            enemy.color = Color.red;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Checkpoint"))
-        {
-            Debug.Log("Saiu da Colisão");
-            enemy.color = Color.yellow;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Checkpoint"))
-        {
-            Debug.Log("Saiu da Colisão");
-            enemy.color = Color.red;
-        }
-    }
-
 }
