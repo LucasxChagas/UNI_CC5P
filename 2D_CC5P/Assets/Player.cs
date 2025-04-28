a83a1ccac698927243c9ef1fa0f083ed5c9bc1d6
Lucas X. C. <contatolchagas@gmail.com>
2025-04-28 01:05:27 -0300

    Aula #4 Tilemap e Desafio

--- 2D_CC5P/Assets/Player.cs
+++ 2D_CC5P/Assets/Player.cs
@@ -3,29 +3,30 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float jumpForce = 15f;
+
     private Vector2 movementInputs;
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private Animator anim;
     private Rigidbody2D rb;
 
     private bool IsWalking;
+    private bool IsOnTheGround;
+
+    // Configuração para estados "IsOnTheAir"
+    private int IsOnTheAirValue;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         GetInputs();
         UpdateAnimator();
         UpdateOrientation();
+        Jump();
     }
 
     private void FixedUpdate()
@@ -35,34 +36,70 @@ public class Player : MonoBehaviour
 
     private void GetInputs()
     {
-        movementInputs = new Vector2(Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical"));
+        // Mudamos os Inputs para mover somente na horizontal, e mantemos a velocidade do personagem em Y;
+        movementInputs = new Vector2(Input.GetAxisRaw("Horizontal"), rb.linearVelocity.y);
     }
 
     private void Move()
     {
-        rb.linearVelocity = movementInputs * movementSpeed;
+        // Acrescentamos velocidade somente em X, e mantemos a velocidade atual do personagem em Y;
+        rb.linearVelocity = new Vector2(movementInputs.x * movementSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Mantemos a velocidade atual do personagem em X, e definimos a velocidade em Y com a força do pulo;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 
     private void UpdateAnimator()
     {
-        if(rb.linearVelocity.magnitude > 0)
+        if (rb.linearVelocity.magnitude > 0) // Está andando...
             IsWalking = true;
-        else
+        else // Está parado...
             IsWalking = false;
 
-        anim.SetBool("IsWalking", IsWalking);
+        // Atualização aérea
+        if (rb.linearVelocity.y > 0.1f && !IsOnTheGround)
+            IsOnTheAirValue = 1;
+        else if (rb.linearVelocity.y < -0.1f && !IsOnTheGround)
+            IsOnTheAirValue = -1;
+        else
+            IsOnTheAirValue = 0;
+
+        // Atualiza os parâmetros no Animator, certifique-se de tê-los criado
+        animator.SetBool("IsWalking", IsWalking);
+        animator.SetInteger("IsOnTheAir", IsOnTheAirValue);
     }
 
     private void UpdateOrientation()
     {
-        if(movementInputs.x > 0) // Está indo para direita...
-        {
+        // Via Rotation...
+        if (movementInputs.x > 0)
             transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-        else if(movementInputs.x < 0) // Está indo para esquerda...
-        {
+        else if(movementInputs.x < 0)
             transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // Certifique-se de criar a tag e atribuir ao nosso objeto "Ground"
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            // Se o personagem colide com o chão, então atualiza a variável;
+            IsOnTheGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            // Se o personagem deixa de colidir com o chão, então atualiza a variável;
+            IsOnTheGround = false;
         }
     }
 }
